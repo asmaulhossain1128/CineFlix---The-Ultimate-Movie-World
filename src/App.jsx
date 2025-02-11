2af77a56c7fc3da9f199af780f5e90aa37143079
asmaulhosen45 <asmaulhosen45@gmail.com>
2025-02-12 00:33:12 +0600

    added Movie Apis with TMDB

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,58 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import HeroBG from "../public/hero.png";
 import Search from "./components/Search";
+import Spinner from "./components/spinner";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMovies = async () => {
+    setErrorMessage("");
+    setIsLoading(true);
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?short_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error("An error occurred. Please try again.");
+      }
+
+      const data = await response.json();
+
+      if (data.response === false) {
+        setErrorMessage(
+          data.error.message || "No movies found. Please try again."
+        );
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || []);
+    } catch (error) {
+      console.log("Error Fetching Movies:", error);
+      setErrorMessage("An error occurred. Please try again.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <main>
       <div className="pattern" />
@@ -16,11 +65,32 @@ const App = () => {
             Find <span className="text-gradient">Movies</span> You&apos;ll Enjoy
             Without the Hassle
           </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <section className="all-movies">
+          <h2 className="mt-10">All Movies</h2>
 
-        <h1 className="text-2xl text-white">{searchTerm}</h1>
+          {isLoading ? (
+            <div className="flex justify-center">
+              <Spinner />
+            </div>
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <li key={movie.id}>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                    alt={movie.title}
+                  />
+                  <p>{movie.title}</p>
+                </li>
+              ))}
+            </ul>
+          )}
+        </section>
       </div>
     </main>
   );
